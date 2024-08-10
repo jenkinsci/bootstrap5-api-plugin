86e73edfb3d14785c4bc653807e87307c73e62a6
Ulli Hafner <ullrich.hafner@gmail.com>
2024-08-10 18:16:35 +0200

    Use correct service name for docker compose.

--- bin/go.sh
+++ bin/go.sh
@@ -11,7 +11,7 @@ cp -fv target/bootstrap5-api.hpi $JENKINS_HOME/plugins/bootstrap5-api.jpi
 
 CURRENT_UID="$(id -u):$(id -g)"
 export CURRENT_UID
-IS_RUNNING=$(docker compose ps -q devenv-jenkins)
+IS_RUNNING=$(docker compose ps -q jenkins)
 if [[ "$IS_RUNNING" != "" ]]; then
     docker compose restart
     echo "Restarting Jenkins (docker compose with user ID ${CURRENT_UID}) ..."
